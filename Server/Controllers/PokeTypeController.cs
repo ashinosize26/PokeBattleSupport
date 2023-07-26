267f3dba81d0f1b5bb4120a72234da73988a0926
tt <ashinosize26@gmail.com>
2023-07-27 02:22:46 +0900

    リファクタリング

---
+++ Server/Controllers/PokeTypeController.cs
@@ -0,0 +1,61 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using PokeBattleSupport.Shared;
+using GrpcPoke;
+
+namespace PokeBattleSupport.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PokeTypeController : ControllerBase
+    {
+        private readonly ILogger<PokeTypeController> _logger;
+
+        public PokeTypeController(ILogger<PokeTypeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<PokeTypeModel> GetAsync([FromQuery] string name)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7191");
+            var client = new PokeSrv.PokeSrvClient(channel);
+            PokeTypes poke = await client.GetPokeTypesAsync(new PokeName() { Name = name });
+
+            return new PokeTypeModel() { Name = poke.Name, No = poke.No, FirstType = poke.FirstType, SecondType = poke.SecondType };
+        }
+
+        [HttpGet("Effective")]
+        public async Task<PokeTypeEffectiveModel> GetEffectiveAsync([FromQuery] string? firstType = null, [FromQuery] string? secondType = null)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7191");
+            var client = new PokeSrv.PokeSrvClient(channel);
+            PokeTypeEffective effectiveValues = await client.GetPokeTypeEffectiveAsync(new PokeTypes() { FirstType = firstType ?? string.Empty, SecondType = secondType ?? string.Empty });
+
+            return new PokeTypeEffectiveModel()
+            {
+                FirstType = firstType ?? string.Empty,
+                SecondType = secondType ?? string.Empty,
+                NormalEffectiveValue = effectiveValues.NormalValue,
+                FireEffectiveValue = effectiveValues.FireValue,
+                WaterEffectiveValue = effectiveValues.WaterValue,
+                ElectricEffectiveValue = effectiveValues.ElecticValue,
+                GrassEffectiveValue = effectiveValues.GrassValue,
+                IceEffectiveValue = effectiveValues.IceValue,
+                FightingEffectiveValue = effectiveValues.FightingValue,
+                PoisonEffectiveValue = effectiveValues.PoisonValue,
+                GroundEffectiveValue = effectiveValues.GroundValue,
+                FlyingEffectiveValue = effectiveValues.FlyingValue,
+                PsychicEffectiveValue = effectiveValues.PsychicValue,
+                BugEffectiveValue = effectiveValues.BugValue,
+                RockEffectiveValue = effectiveValues.RockValue,
+                GhostEffectiveValue = effectiveValues.GhostValue,
+                DragonEffectiveValue = effectiveValues.DragonValue,
+                DarkEffectiveValue = effectiveValues.DarkValue,
+                SteelEffectiveValue = effectiveValues.SteelValue,
+                FairyEffectiveValue = effectiveValues.FairyValue,
+            };
+        }
+    }
+}
--- Shared/PokeTypeEffectiveModel.cs
+++ Shared/PokeTypeEffectiveModel.cs
@@ -12,41 +12,41 @@ namespace PokeBattleSupport.Shared
 
         public string SecondType { get; set; } = string.Empty;
 
-        public double NormalEffectiveValue { get; set; }
+        public double NormalEffectiveValue { get; set; } = 1;
 
-        public double FireEffectiveValue { get; set; }
+        public double FireEffectiveValue { get; set; } = 1;
 
-        public double WaterEffectiveValue { get; set; }
+        public double WaterEffectiveValue { get; set; } = 1;
 
-        public double ElectricEffectiveValue { get; set; }
+        public double ElectricEffectiveValue { get; set; } = 1;
 
-        public double GrassEffectiveValue { get; set; }
+        public double GrassEffectiveValue { get; set; } = 1;
 
-        public double IceEffectiveValue { get; set; }
+        public double IceEffectiveValue { get; set; } = 1;
 
-        public double FightingEffectiveValue { get; set; }
+        public double FightingEffectiveValue { get; set; } = 1;
 
-        public double PoisonEffectiveValue { get; set; }
+        public double PoisonEffectiveValue { get; set; } = 1;
 
-        public double GroundEffectiveValue { get; set; }
+        public double GroundEffectiveValue { get; set; } = 1;
 
-        public double FlyingEffectiveValue { get; set; }
+        public double FlyingEffectiveValue { get; set; } = 1;
 
-        public double PsychicEffectiveValue { get; set; }
+        public double PsychicEffectiveValue { get; set; } = 1;
 
-        public double BugEffectiveValue { get; set; }
+        public double BugEffectiveValue { get; set; } = 1;
 
-        public double RockEffectiveValue { get; set; }
+        public double RockEffectiveValue { get; set; } = 1;
 
-        public double GhostEffectiveValue { get; set; }
+        public double GhostEffectiveValue { get; set; } = 1;
 
-        public double DragonEffectiveValue { get; set; }
+        public double DragonEffectiveValue { get; set; } = 1;
 
-        public double DarkEffectiveValue { get; set; }
+        public double DarkEffectiveValue { get; set; } = 1;
 
-        public double SteelEffectiveValue { get; set; }
+        public double SteelEffectiveValue { get; set; } = 1;
 
-        public double FairyEffectiveValue { get; set; }
+        public double FairyEffectiveValue { get; set; } = 1;
 
     }
 }
